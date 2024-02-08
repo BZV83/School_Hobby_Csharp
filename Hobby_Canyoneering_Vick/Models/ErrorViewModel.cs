a1a15dd322eca27f87b50f06326376fe6e0327ff
BZV83 <bzvspry@gmail.com>
2024-02-07 19:28:55 -0700

    Add project files.

---
+++ Hobby_Canyoneering_Vick/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Hobby_Canyoneering_Vick.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Hobby_Canyoneering_Vick/wwwroot/js/site.js
@@ -0,0 +1,22 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+function calculateTotal() {
+    var hours = $('#numHours').val();
+    var totalPrice = 0;
+    var OutputText = '';
+
+    if (hours < 0) {
+        alert('Invalid Input. Enter a positive number.')
+    }
+    else {
+        totalPrice = hours * 35;
+    }
+
+    totalPrice = Math.round(totalPrice, 2);
+
+    OutputText = "Your total expected price is $" + totalPrice.toString() + ".";
+
+    $('#output').html(OutputText);
+}
