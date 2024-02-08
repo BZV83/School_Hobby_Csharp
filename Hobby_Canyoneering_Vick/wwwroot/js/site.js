6318240781f35ed0f00dd4e25d5b3dbe6448020e
BZV83 <bzvspry@gmail.com>
2024-02-07 19:41:30 -0700

    fixed some input things

--- Hobby_Canyoneering_Vick/wwwroot/js/site.js
+++ Hobby_Canyoneering_Vick/wwwroot/js/site.js
@@ -4,19 +4,17 @@
 // Write your JavaScript code.
 function calculateTotal() {
     var hours = $('#numHours').val();
-    var totalPrice = 0;
+    var totalPrice = 0.0;
     var OutputText = '';
 
     if (hours < 0) {
         alert('Invalid Input. Enter a positive number.')
     }
     else {
-        totalPrice = hours * 35;
+        totalPrice = Math.round(hours * 35,2);
+
+        OutputText = "Your total expected price is $" + totalPrice.toString() + ".";
+
+        $('#output').html(OutputText);
     }
-
-    totalPrice = Math.round(totalPrice, 2);
-
-    OutputText = "Your total expected price is $" + totalPrice.toString() + ".";
-
-    $('#output').html(OutputText);
 }
